22a147e4cfd469b312dfde1a949573f84a5cc9ef
GUI Wuff <gui.wuff@gmail.com>
2017-03-06 06:42:12 +0700

    Create Tutorial File
    Tutorial.md

---
+++ Tutorial.md
@@ -0,0 +1,7 @@
+# Membangun integrasi DNS Query dengan Slack menggunakan PHP
+
+### Tambahkan Konfigurasi Slash Command di Slack
+
+- Masuk ke dalam Direktori Aplikasi Slack dan Search Slash Command. Add Configuration untuk Slash Commands, dalam hal ini yang akan kita register adalah command /dns
+- Masuk ke bagian konfigurasi dan pelajari data yang akan dikirim dari Slack kepada url yang kita tentukan
+- 
--- php-dns-query/index.php
+++ php-dns-query/index.php
@@ -4,5 +4,30 @@
 / PHP Server Application to listen to DNS Query coming from Slack
 ***************/
 
+// Config
 
+$payload_token = 'ZdGvnv3KUSXAB7GiUEsWTZMw';
+
+
+$command = $_POST['command'];
+$text = $_POST['text'];
+$token = $_POST['token'];
+
+// Check Token 
+
+if($token != $payload_token){ 
+    $msg = "Token yang digunakan tidak valid, silahkan periksa kembali konfigurasi anda";
+    die($msg);
+    echo $msg;
+}
+
+switch ($text) {
+    case 'whois':
+        $msg = "Whois";
+        echo $msg;
+        break;
+    default:
+        # code...
+        break;
+}
 ?>
